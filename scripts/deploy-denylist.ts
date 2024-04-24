e84be7748036f1307061947cc45696b58ba3b065
jayt106 <jay.tseng@crypto.com>
2024-04-24 15:53:48 -0400

    add updatelist tooling

--- scripts/deploy-denylist.ts
+++ scripts/deploy-denylist.ts
@@ -6,6 +6,7 @@ import { Command } from "commander";
 import { Contract, Wallet, ethers } from "ethers";
 
 import "dotenv/config"
+import * as util from './util';
 
 async function main() {
   const program = new Command();
@@ -17,14 +18,7 @@ async function main() {
     .option("--contract <contractName>")
     .option("--args <contract-constructor-arguments>")
     .action(async (cmd) => {
-      const deployWallet = cmd.privateKey
-        ? new Wallet(cmd.privateKey, provider)
-        : Wallet.fromMnemonic(
-          cmd.mneminic ? cmd.mneminic : process.env.MNEMONIC,
-            "m/44'/60'/0'/0/1"
-          ).connect(provider);
-      console.log(`Using deployer wallet: ${deployWallet.address}`);
-
+      const deployWallet = util.getWallet(cmd);
       await contractDeployment(deployWallet, cmd);
     });
 
@@ -53,32 +47,6 @@ async function contractDeployment(
   console.log(`CONTRACTS_DEPLOYED_TXHASH=${receipt.transactionHash}`);
 }
 
-const provider = web3Provider();
-
-function web3Url() {
-  return process.env.ETH_CLIENT_WEB3_URL;
-}
-
-function web3Provider() {
-  const provider = new ethers.providers.JsonRpcProvider(web3Url());
-
-  // Check that `CHAIN_ETH_NETWORK` variable is set. If not, it's most likely because
-  // the variable was renamed. As this affects the time to deploy contracts in localhost
-  // scenario, it surely deserves a warning.
-  const network = process.env.CHAIN_ETH_NETWORK;
-  if (!network) {
-    console.log(warning("Network variable is not set. Check if process env CHAIN_ETH_NETWORK is set"));
-  }
-
-  // Short polling interval for local network
-  if (network === "localhost" || network === "hardhat") {
-    provider.pollingInterval = 100;
-  }
-
-  return provider;
-}
-
-
 main()
   .then(() => process.exit(0))
   .catch((err) => {
@@ -86,8 +54,4 @@ main()
     process.exit(1);
   });
 
-
-function warning(arg0: string): any {
-  throw new Error(arg0);
-}
 
