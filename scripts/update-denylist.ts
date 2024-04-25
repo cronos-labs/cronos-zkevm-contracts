fd824f30bf4b205283974aeb808dfa2b79077cfc
JayT106 <JayT106@users.noreply.github.com>
2024-04-25 10:51:06 -0400

    Update scripts/update-denylist.ts
    Co-authored-by: Thomas Nguy <81727899+thomas-nguy@users.noreply.github.com>

--- scripts/update-denylist.ts
+++ scripts/update-denylist.ts
@@ -35,7 +35,7 @@ async function main() {
         for (const addr of list) {
           const newargs = [addr, addr, 0, 0, 0, addr];
           const allowed = await CONTRACT.isTransactionAllowed(...newargs);
-          console.log("The tx from address:", addr, "is", allowed? "allowed": "not allowed");
+          console.log("Txs from address:", addr, "are", allowed? "allowed": "not allowed");
         }
       });
   
