0de64c16070cb875181510e8d0d8011ef00a99d2
JayT106 <JayT106@users.noreply.github.com>
2024-04-25 20:55:09 -0400

    Problem: missing tooling for updating the deny list of TransactionFiltererDenyList (#6)
    Co-authored-by: Thomas Nguy <81727899+thomas-nguy@users.noreply.github.com>

---
+++ scripts/update-denylist.ts
@@ -0,0 +1,50 @@
+// hardhat import should be the first import in the file
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as hardhat from "hardhat";
+import "@nomiclabs/hardhat-ethers";
+import { Command } from "commander";
+
+import "dotenv/config"
+import * as util from './util';
+
+
+async function main() {
+    const program = new Command();
+    program.version("0.1.0").name("deny-list-update").description("update denylist");
+  
+    program
+      .option('--private-key <private-key>')
+      .option('--mnemonic <mnemonic>')
+      .option('--remove', 'remove address')
+      .requiredOption('--list <addresses-to-be-added-to-deny-list>')
+      .requiredOption("--contract <contract-address>")
+      .action(async (cmd) => {
+        const wallet = util.getWallet(cmd);
+
+        var args: any[] = [];
+        const list = cmd.list ? cmd.list.split(",") : [];
+        const add = cmd.remove ? false : true;
+        args = [list, add];
+
+        const CONTRACT = await hardhat.ethers.getContractAt("TransactionFiltererDenyList", cmd.contract, wallet);
+        console.log(`The input args of updateDenyList: ${args}`);
+        const tx = await CONTRACT.updateDenyList(...args);
+        const receipt = await tx.wait();
+        console.log("Tx hash:", receipt.transactionHash);
+
+        for (const addr of list) {
+          const newargs = [addr, addr, 0, 0, 0, addr];
+          const allowed = await CONTRACT.isTransactionAllowed(...newargs);
+          console.log("Txs from address:", addr, "are", allowed? "allowed": "not allowed");
+        }
+      });
+  
+    await program.parseAsync(process.argv);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error("Error:", err);
+    process.exit(1);
+  });        
---
+++ scripts/util.ts
@@ -0,0 +1,43 @@
+import { Wallet, ethers } from "ethers";
+
+export const provider = web3Provider();
+
+export function web3Url() {
+  return process.env.ETH_CLIENT_WEB3_URL;
+}
+
+export function web3Provider() {
+  const provider = new ethers.providers.JsonRpcProvider(web3Url());
+
+  // Check that `CHAIN_ETH_NETWORK` variable is set. If not, it's most likely because
+  // the variable was renamed. As this affects the time to deploy contracts in localhost
+  // scenario, it surely deserves a warning.
+  const network = process.env.CHAIN_ETH_NETWORK;
+  if (!network) {
+    console.log(warning("Network variable is not set. Check if process env CHAIN_ETH_NETWORK is set"));
+  }
+
+  // Short polling interval for local network
+  if (network === "localhost" || network === "hardhat") {
+    provider.pollingInterval = 100;
+  }
+
+  return provider;
+}
+
+function warning(arg0: string): any {
+    throw new Error(arg0);
+}
+
+export function getWallet(cmd: any): Wallet {
+    const wallet = cmd.privateKey
+    ? new Wallet(cmd.privateKey, provider)
+    : Wallet.fromMnemonic(
+      cmd.mneminic ? cmd.mneminic : process.env.MNEMONIC,
+        "m/44'/60'/0'/0/1"
+      ).connect(provider);
+  console.log(`Using wallet: ${wallet.address}`);
+  return wallet;
+}
+  
+
