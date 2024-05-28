c0dc46cb7fed601e52c98343df7cc6d6fef87ab8
jayt106 <jay.tseng@crypto.com>
2024-05-28 09:55:18 -0400

    add input list length check in DenyList contract

--- contracts/test/foundry/unit/denylist/TransactionFiltererDenyList.t.sol
+++ contracts/test/foundry/unit/denylist/TransactionFiltererDenyList.t.sol
@@ -48,4 +48,33 @@ contract TransactionFiltererDenyListTest is BaseTest {
         allowed = td.isTransactionAllowed(address(s), address(0), 0, 0, "", address(0));
         assertTrue(allowed == false, "contract should denies the tx sender");
     }
+
+    function test_denylist_init_revert() public {
+        address owner = makeAddr("owner");
+        address[] memory denylist = new address[](0);
+
+        address s = makeAddr("sender");
+        vm.startPrank(s);
+        vm.expectRevert();
+
+        new TransactionFiltererDenyList(owner, denylist);
+    }
+
+    function test_denylist_update_denylist_revert() public {
+        address owner = makeAddr("owner");
+        address[] memory denylist = new address[](1);
+        address s = makeAddr("sender");
+        address s1 = makeAddr("sender1");
+        denylist[0] = s1;
+        TransactionFiltererDenyList td = new TransactionFiltererDenyList(owner, denylist);
+
+        bool allowed = td.isTransactionAllowed(address(s), address(0), 0, 0, "", address(0));
+        assertTrue(allowed == true, "contract should allows the sender");
+
+        address[] memory update_denylist = new address[](0);
+        vm.deal(owner, 1 ether);
+        vm.prank(address(owner));
+        vm.expectRevert();
+        td.updateDenyList(update_denylist, true);
+    }
 }
